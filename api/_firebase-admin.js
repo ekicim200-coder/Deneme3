67b443d177a1c58d8420ea55745cbd9050179a60
ekicim200-coder <ekicim200@gmail.com>
2026-03-03 23:58:10 +0300

    Update _firebase-admin.js

--- api/_firebase-admin.js
+++ api/_firebase-admin.js
@@ -1,1 +1,20 @@
+const admin = require('firebase-admin');
 
+let db, initError;
+
+try {
+    if (!admin.apps.length) {
+        admin.initializeApp({
+            credential: admin.credential.cert({
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            }),
+        });
+    }
+    db = admin.firestore();
+} catch (e) {
+    initError = e.message;
+}
+
+module.exports = { db, initError };
